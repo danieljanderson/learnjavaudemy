6718fafb094d7a2526b3dc942e814c83ef39bbca
Daniel Anderson <danieljanderson0616@gmail.com>
2021-12-01 13:35:18 -0500

    forgot to push yesturdays session

--- exercises/helloWorld/src/com/github/helloworld/HelloWorld.java
+++ exercises/helloWorld/src/com/github/helloworld/HelloWorld.java
@@ -4,5 +4,24 @@ public class HelloWorld {
     public static void main(String[]args){
         System.out.println("hello world");
 
+
+        String myName = "Daniel";
+        String myDog = "Maci";
+        String myMom = "Charisse";
+
+        int myNumber = 12;
+        int anotherNumber = 34;
+
+        System.out.println(myNumber);
+        System.out.println(anotherNumber);
+        System.out.println(myName+ " has a dog named: "+myDog);
+        System.out.println(myName+(myNumber+anotherNumber)+" dollars");
+
+        String lastName = "Anderson";
+
+        int myAge = 34;
+
+        System.out.println("Hi my name is " + lastName + " and I am "+ myAge);
+
     }
 }
