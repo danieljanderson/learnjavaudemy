d57e6e48883a6d699d7cb4791d477f2dc9f720cc
Daniel Anderson <danieljanderson0616@gmail.com>
2021-12-03 13:24:04 -0500

    wednesday

---
+++ exercises/BasicOps/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //Additon = +
+        int firstNum = 34;
+        int secondNum = 3;
+
+        System.out.println("the sum of " + firstNum + " and "+ secondNum+ " is "+(firstNum+secondNum));
+    }
+}
